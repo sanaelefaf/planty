1f525d34dc62dc3a4da32551c36765ed9634a04f
sanaa <sanaa.lefaf@gmail.com>
2023-05-09 22:19:27 +0200

    formulaire commande

--- wp-content/themes/oceanwp-child-theme-master/functions.php
+++ wp-content/themes/oceanwp-child-theme-master/functions.php
@@ -50,3 +50,4 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
